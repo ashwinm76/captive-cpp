561ab1d2f1f52ef1fb1e794db5336d2ebbd271ba
Ashwin Menon <ashwin.menon@gmail.com>
2023-03-14 17:49:31 +0530

    Simplify test fixture.

--- tests/test_fixture.cc
+++ tests/test_fixture.cc
@@ -17,14 +17,42 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include <fstream>
+#include <iostream>
 #include <vector>
 
 #include "captive.h"
 #include "iotest.h"
 
 int main(int argc, char **argv) {
-  IoTest io({"kick door", "n", "ring bell"},  // die here,
-            "You have woken the dead who do not like you too much.");
+  std::vector<std::string> input;
+  std::string output;
+  std::ifstream ifile;
+
+  if (argc != 2) {
+    std::cout << "No stimulus file passed" << std::endl;
+    return 1;
+  }
+
+  ifile.open(argv[1]);
+  if (ifile.is_open()) {
+    std::string line;
+    while (std::getline(ifile, line)) {
+      if (line[0] != '#' && line.size() > 0) {
+        input.push_back(line);
+      }
+    }
+    ifile.close();
+    if (input.size() > 0) {
+      output = input.back();
+      input.pop_back();
+    }
+  } else {
+    std::cout << "Can't open " << argv[1] << std::endl;
+    return 1;
+  }
+
+  IoTest io(input, output);
   Captive captive(&io);
 
   captive.Run();
